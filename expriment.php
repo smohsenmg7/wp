51f6a4e9a3ab309267c2704123da4678a3199371
smohsenmg7 <smohsenmg7@gmail.com>
2021-08-29 08:31:10 +0430

    my theme

---
+++ expriment.php
@@ -0,0 +1,7 @@
+<?php 
+$name= 'mohsen';
+
+
+?>
+
+<h1>hello  <?php echo $name ?></h1 >
---
+++ wp-content/themes/mohsentheme/index.php
@@ -0,0 +1,7 @@
+<?php 
+function showing ($name,$color)  
+{
+    echo "<p>hi my name is $name and my color is $color</p>";
+}
+
+?>
