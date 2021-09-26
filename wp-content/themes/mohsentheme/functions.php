edd10f3f8b4b66912a74554dcf125be765de254e
smohsenmg7 <smohsenmg7@gmail.com>
2021-09-26 22:37:31 +0330

    wp

--- wp-content/themes/mohsentheme/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-function my_wp_files() {
-	wp_enqueue_style( 'main_style', get_stylesheet_uri() );
-}
-
-add_action( 'wp_enqueue_scripts', 'my_wp_files' );
--- wp-content/themes/mohsentheme/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<?php wp_head(); ?>
-</head>
-<body>
-<h1>header panel</h1>
-</body>
-</html>
