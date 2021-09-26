bcf4c1261df7308145dd98a3d33134046047a827
smohsenmg7 <smohsenmg7@gmail.com>
2021-09-26 04:59:30 +0330

    wp

--- wp-content/themes/mohsentheme/index.php
+++ wp-content/themes/mohsentheme/index.php
@@ -2,13 +2,5 @@
 get_header();
 $count = 0;
 while ( have_posts() ) {
-	the_post();
-	?>
-    <h2><a href="<?php the_permalink(); ?>"> <?php the_title() ?> </a></h2>
-	<?php the_content(); ?>
 
-	<?php
 }
-get_footer()   ;
-?>
-
